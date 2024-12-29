b8280636fb8b6c378b0480e50b974306af235510
Jorge Blanco Alonso <jorge.blancoalonso@epfl.ch>
2024-12-29 21:19:54 +0100

    Adapt example for the api changes

--- example/basic/basic.ino
+++ example/basic/basic.ino
@@ -4,16 +4,34 @@
 const char* TEST_PUBLIC_KEY = "1234567890123456789012345678901234567890123456789012345678901234";
 const char* TEST_PRIVATE_KEY = "1234567890123456789012345678901234567890123456789012345678901234";
 const char* TEST_NODE_ID = "test_node_001";
+const char* TEST_SERVER_URL = "http://kda.crankk.org/chainweb/0.0/mainnet01/chain/19/pact/api/v1/";
 
 static int32_t callBlockchain() {
+    // Packet ID generator callback
     auto packetIdGen = []() -> uint32_t {
         static uint32_t id = 0;
         return ++id;
     };
 
-    std::unique_ptr<BlockchainHandler> blockchainHandler(
-        new BlockchainHandler(TEST_PUBLIC_KEY, TEST_PRIVATE_KEY, true, packetIdGen));
-    return blockchainHandler->performNodeSync(TEST_NODE_ID);
+    // Secret generation callback
+    auto onSecretGen = [](uint32_t packetId) {
+        Serial.printf("Generated secret for packet: %u\n", packetId);
+    };
+
+    // Create blockchain handler instance
+    BlockchainHandler blockchainHandler(
+        TEST_PUBLIC_KEY,
+        TEST_PRIVATE_KEY,
+        true,  // Enable wallet
+        TEST_SERVER_URL
+    );
+
+    // Perform node sync
+    return blockchainHandler.performNodeSync(
+        TEST_NODE_ID,
+        packetIdGen,
+        onSecretGen
+    );
 }
 
 void setup() {
